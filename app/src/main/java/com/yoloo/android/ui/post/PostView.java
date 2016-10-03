c8797382676cdbf29a4b46f2c95fdcab88ac1f50
Yasin Sinan Kayacan <yasinsinan707@gmail.com>
2016-10-03 16:01:19 +0300

    Close to v1

--- app/src/main/java/com/yoloo/android/ui/post/PostView.java
+++
@@ -1,8 +0,0 @@
-package com.yoloo.android.ui.post;
-
-import com.yoloo.android.backend.modal.yolooApi.model.CollectionResponseAbstractPost;
-import com.yoloo.android.framework.base.BaseMvpDataView;
-
-public interface PostView extends BaseMvpDataView<CollectionResponseAbstractPost> {
-
-}
